e1285884682ec9a1a92efbc458c28e26d6166c73
vinith <vinithkouda@gmail.com>
2023-10-07 14:49:51 +0530

    Adding license and syncfusion components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { DataBindingService, DiagramModule, HierarchicalTreeService } from '@syncfusion/ej2-angular-diagrams';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavigationComponent } from './side-navigation/side-navigation.component';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1JpQXxbf1xzZFREalhZTnRZUj0eQnxTdEZiWX9bcXZRTmJbU0d2XA==');
 
 @NgModule({
   declarations: [
     AppComponent,
-    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SideNavigationComponent
+    SideNavigationComponent,
+    DiagramModule
   ],
-  providers: [],
+  providers: [HierarchicalTreeService, DataBindingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
